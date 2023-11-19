3f518d144b65768380b3f93389cfb58c2f9820be
Adzhi4ka <y88005553535@yandex.ru>
2023-11-20 02:43:24 +0300

    fix

--- circle.py
+++ circle.py
@@ -33,3 +33,47 @@ def perimeter(r):
             perimeter(0.5) = 3.14159256
     '''
     return 2 * math.pi * r
+
+class CircleTestCase(unittest.TestCase):
+
+    def test_positive_integer_radius_area(self):
+        res = area(10)
+        self.assertEqual(res, 314.1592653589793)
+    def test_negative_integer_radius_area(self):
+        res = area(-10)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_positive_integer_radius_perimetr(self):
+        res = perimeter(10)
+        self.assertEqual(res, 62.83185307179586)
+    def test_negative_integer_radius_perimetr(self):
+        res = perimeter(-10)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_positive_float_radius_area(self):
+        res = area(2.5)
+        self.assertEqual(res, 19.634954084936208)
+    def test_negative_float_radius_area(self):
+        res = area(-2.5)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_positive_float_radius_perimetr(self):
+        res = perimeter(c)
+        self.assertEqual(res, 15.707963267948966)
+    def test_negative_float_radius_perimetr(self):
+        res = perimeter(-2.5)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_string_radius_area(self):
+        res = area("XDXDXD")
+        self.assertEqual(res, "UncorrectValueType")
+    def test_string_radius_perimetr(self):
+        res = perimeter("XDXDXD")
+        self.assertEqual(res, "UncorrectValueType")
+
+    def test_big_int_radius_area(self):
+        res = area(10**20)
+        self.assertEqual(res, 3.141592653589793e+40)
+    def test_big_int_radius_perimetr(self):
+        res = perimeter(5 * 10**19)
+        self.assertEqual(res, 3.1415926535897933e+20)
--- square.py
+++ square.py
@@ -32,3 +32,46 @@ def perimeter(a):
                 perimeter(0.5) = 2
     '''
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+    def test_positive_integer_side_area(self):
+        res = area(2)
+        self.assertEqual(res, 4)
+    def test_negative_integer_sides_area(self):
+        res = area(-2)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_positive_integer_side_perimeter(self):
+        res = perimeter(2)
+        self.assertEqual(res, 8)
+    def test_negative_integer_side_perimeter(self):
+        res = perimeter(-2)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_positive_float_side_area(self):
+        res = area(2.5)
+        self.assertEqual(res, 6.25)
+    def test_negative_float_side_area(self):
+        res = area(-2.5)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_positive_float_side_perimeter(self):
+        res = perimeter(2.5)
+        self.assertEqual(res, 10)
+    def test_negative_float_sides_perimeter(self):
+        res = perimeter(-2.5)
+        self.assertEqual(res, "UncorrectValue")
+
+    def test_big_integer_area(self):
+        res = area(10**40)
+        self.assertEqual(res, 10**80)
+    def test_big_integer_perimeter(self):
+        res = perimeter(10**40)
+        self.assertEqual(res, 4 * 10**40)
+    
+    def test_string_area(self):
+        res = area("xd")
+        self.assertEqual(res, "UncorrectValueType")
+    def test_string_perimeter(self):
+        res = perimeter("xd")
+        self.assertEqual(res, "UncorrectValueType")
